e5948a12aad1f63f9244855dd3da84d93f1db303
chenpengfei <alffei@163.com>
2020-08-05 15:41:06 +0800

    add SwitchTest

--- java-style-guide/src/test/java/com/gd/talks/one/SwitchTest.java
+++ java-style-guide/src/test/java/com/gd/talks/one/SwitchTest.java
@@ -1,4 +1,15 @@
 package com.gd.talks.one;
 
 public class SwitchTest {
+
+    public static void main(String[] args) {
+        String param = "1";
+        switch (param) {
+            case "1":
+                System.out.println("1");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
 }
